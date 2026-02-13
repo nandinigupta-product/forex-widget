f24a5b634f4f0903d3e897ff70650b12c882dacd
nandinigupta201 <46275341-nandinigupta201@users.noreply.replit.com>
2026-02-13 09:18:02 +0000

    Restored to '4a1fb225ffc80d3928d1e32727d6d7d38b1eae02'
    Replit-Restored-To: 4a1fb225ffc80d3928d1e32727d6d7d38b1eae02

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,7 +2,7 @@ import { ForexWidget } from "@/components/ForexWidget";
 
 export default function Home() {
   return (
-    <div className="w-full min-h-screen flex items-start justify-center md:p-4 p-0">
+    <div className="w-full flex items-start justify-center p-4">
       <ForexWidget />
     </div>
   );
